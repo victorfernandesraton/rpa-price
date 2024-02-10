75ce5769782b3a251b2e4b63f55098b75f869a7e
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-02-10 13:58:29 -0300

    fix: improve better imports

--- rpa-price/rpa.py
+++ rpa-price/rpa.py
@@ -1,6 +1,7 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.support.wait import WebDriverWait, time
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
